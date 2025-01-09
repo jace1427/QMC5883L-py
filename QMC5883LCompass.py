cde17be66afdbcce82eb360a90db79299c2f0b7e
Justin Spidell <jspidelll@gmail.com>
2025-01-08 16:28:20 -0800

    first test

--- QMC5883LCompass.py
+++ QMC5883LCompass.py
@@ -1,43 +1,59 @@
+import logging
+import time
 
-import smbus2
+from smbus2 import SMBus
 
-class QMC5883L():
-    def __init__(self) -> None:
-         pass
+import constants as c
+
+
+class QMC5883L:
+    def __init__(self, output_range=c.RNG_2G, address=c.DFLT_ADDRESS) -> None:
+        self.bus = SMBus(1) # TODO - not sure why bus 1 but it seems right
+        self.output_range = output_range
+        self.addr = address
+
+    def _read_data(self, low, high):
+        low = self.bus.read_byte_data(self.addr, low)
+        high = self.bus.read_byte_data(self.addr, high)
+        ret = (high << 8) + low
+        if ret >= 0x8000: 
+            return ret - 0x10000
+        else:
+            return ret
 
     def read(self):
-        pass
-#        """Read data from magnetic and temperature data registers."""
-#        i = 0
-#        [x, y, z, t] = [None, None, None, None]
-#        while i < 20:  # Timeout after about 0.20 seconds.
-#            status = self._read_byte(REG_STATUS_1)
-#            if status & STAT_OVL:
-#                # Some values have reached an overflow.
-#                msg = ("Magnetic sensor overflow.")
-#                if self.output_range == RNG_2G:
-#                    msg += " Consider switching to RNG_8G output range."
-#                logging.warning(msg)
-#            if status & STAT_DOR:
-#                # Previous measure was read partially, sensor in Data Lock.
-#                x = self._read_word_2c(REG_XOUT_LSB)
-#                y = self._read_word_2c(REG_YOUT_LSB)
-#                z = self._read_word_2c(REG_ZOUT_LSB)
-#                continue
-#            if status & STAT_DRDY:
-#                # Data is ready to read.
-#                x = self._read_word_2c(REG_XOUT_LSB)
-#                y = self._read_word_2c(REG_YOUT_LSB)
-#                z = self._read_word_2c(REG_ZOUT_LSB)
-#                t = self._read_word_2c(REG_TOUT_LSB)
-#                break
-#            else:
-#                # Waiting for DRDY.
-#                time.sleep(0.01)
-#                i += 1
-#        return [x, y, z, t]
-#
-if __name__ == '__main__':
-    test = QMC5883L()
-    print("hiya!")
+        """ """
+        i = 20
+        [x, y, z, t] = [None, None, None, None]
+        while i < 20:  # Timeout after about 0.20 seconds.
+            status = self.bus.read_byte_data(self.addr, c.REG_STATUS_1)
+            if status & c.STAT_OVL:
+                # Some values have reached an overflow.
+                msg = "Magnetic sensor overflow."
+                if self.output_range == c.RNG_2G:
+                    msg += " Consider switching to RNG_8G output range."
+                logging.warning(msg)
+            if status & c.STAT_DOR:
+                # Previous measure was read partially, sensor in Data Lock.
+                x = self._read_data(c.REG_XOUT_LSB, c.REG_XOUT_MSB)
+                y = self._read_data(c.REG_YOUT_LSB, c.REG_YOUT_MSB)
+                z = self._read_data(c.REG_ZOUT_LSB, c.REG_ZOUT_MSB)
+                continue
+            if status & c.STAT_DRDY:
+                # Data is ready to read.
+                x = self._read_data(c.REG_XOUT_LSB, c.REG_XOUT_MSB)
+                y = self._read_data(c.REG_YOUT_LSB, c.REG_YOUT_MSB)
+                z = self._read_data(c.REG_ZOUT_LSB, c.REG_ZOUT_MSB)
+                t = self._read_data(c.REG_TOUT_LSB, c.REG_TOUT_MSB)
+                break
+            else:
+                # Waiting for DRDY.
+                time.sleep(0.01)
+                i += 1
+        print(f"X: {x}, Y: {y}, Z: {z}, T: {t},")
+        return [x, y, z, t]
 
+
+if __name__ == "__main__":
+    compass = QMC5883L()
+    compass.read()
---
+++ constants.py
@@ -0,0 +1,57 @@
+################
+## CONSTANTS  ##
+################
+DFLT_BUS = 1
+DFLT_ADDRESS = 0x0d
+
+#### Registers ####
+# Data Output
+REG_XOUT_LSB = 0x00
+REG_XOUT_MSB = 0x01
+REG_YOUT_LSB = 0x02
+REG_YOUT_MSB = 0x03
+REG_ZOUT_LSB = 0x04
+REG_ZOUT_MSB = 0x05
+
+# Status
+REG_STATUS_1 = 0x06
+
+# Temperature Output
+REG_TOUT_LSB = 0x07
+REG_TOUT_MSB = 0x08
+
+# Configuration
+REG_CONTROL_1 = 0x09    # Control register 1
+REG_CONTROL_2 = 0x0a    # Control register 2
+REG_RST_PERIOD = 0x0b   # SET/RESET period register
+REG_CHIP_ID = 0x0d      # ChipID register
+
+#### Flags for status register 1 ####
+STAT_DRDY = 0b00000001  # Data ready
+STAT_OVL = 0b00000010   # Overflow
+STAT_DOR = 0b00000100   # Data skipped for reading
+
+#### Flags for control register 1 ####
+# Mode control
+MODE_STBY = 0b00000000  # Standby mode
+MODE_CONT = 0b00000001  # Continuous read mode
+# Output Data Rate
+ODR_10HZ = 0b00000000
+ODR_50HZ = 0b00000100
+ODR_100HZ = 0b00001000
+ODR_200HZ = 0b00001100
+# Full scale
+RNG_2G = 0b00000000     # Range 2 Gauss: for magnetic-clean environments
+RNG_8G = 0b00010000     # Range 8 Gauss: for strong magnetic fields
+# Over sample ratio
+OSR_512 = 0b00000000    # 512: less noise, more power
+OSR_256 = 0b01000000
+OSR_128 = 0b10000000
+OSR_64 = 0b11000000     # 64: more noise, less power
+
+#### Flags for control register 2 ####
+INT_ENB = 0b00000001    # Disable interrupt PIN
+POL_PNT = 0b01000000    # Enable pointer roll-over function
+SOFT_RST = 0b10000000   # Soft reset, restore default value of all registers
+
+
